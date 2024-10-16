07196ad4831ed178408949047f95bbcae5f8dace
GARVIT28-CHARAN <garvitcharan000@gmail.com>
2024-10-16 19:47:04 +0530

    stored product description

---
+++ ecartapp/store/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+from category.models import Category
+
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200,unique=True)
+    slug         = models.SlugField(max_length=200,unique=True)
+    description  = models.TextField(max_length=500,blank=True)
+    price        = models.IntegerField()
+    images       = models.ImageField(upload_to='photos/products')
+    stock        = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category     = models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
+    
+    '''
+         auto_now updates the field value every time the instance is saved, while auto_now_add sets the field value only when the instance is created
+    '''
+variation_category_choice=(
+    ('color','color'),
+    ('size','size')
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100,choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+
+
+    def __unicode__(self):
+        return self.product
